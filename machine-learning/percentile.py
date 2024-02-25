d9c0a0d497422a8a4ea7fe9c5ec64c0ccdc7c5f6
Ulus Ata <148704970+ulusata@users.noreply.github.com>
2024-02-25 17:57:07 +0300

    Update percentile.py

--- machine-learning/percentile.py
+++ machine-learning/percentile.py
@@ -6,7 +6,7 @@ ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
 
 percentileValue = np.percentile(ages, 75)
 print(percentileValue) #43
-#It means that %75 of people are 43 are lower than that.
+#It means that %75 of people are 43 and lower than that.
 
 percentileValue = np.percentile(ages, 90)
 print(percentileValue) #61
